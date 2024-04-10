af3ac39b85678629e3cd5d39c7f66808b5b42334
itsmedapny <madaphneeevediente@gmail.com>
2024-04-10 12:40:13 +0800

    Student update view

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView, OrganizationMemberList, OrganizationMemberCreateView, OrganizationMemberUpdateView, OrganizationMemberDeleteView, StudentList, StudentCreateView
+from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView, OrganizationMemberList, OrganizationMemberCreateView, OrganizationMemberUpdateView, OrganizationMemberDeleteView, StudentList, StudentCreateView, StudentUpdateView
 from studentorg import views
 
 urlpatterns = [
@@ -32,4 +32,5 @@ urlpatterns = [
     path('organization_mem_list/<pk>/delete', OrganizationMemberDeleteView.as_view(), name='organization-mem-delete'), 
     path('student_list', StudentList.as_view(), name='student-list'), 
     path('student_list/add', StudentCreateView.as_view(), name='student-add'),
+    path('student_list/<pk>', StudentUpdateView.as_view(), name='student-update'), 
 ]
